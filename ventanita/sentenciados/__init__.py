c7bd1b7f989bc799ed3c44bf1e118a627a2b0c13
aniversarioperu <yoni@aniversarioperu.me>
2015-03-20 23:46:56 +0200

    copyright in sentenciados

---
+++ ventanita/sentenciados/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2015 by AniversarioPeru. All rights reserved.
+# This code is part of the Ventanita distribution and governed by its
+# license. Please see the LICENSE file that should have been included
+# as part of this package.
--- ventanita/sentenciados/tests.py
+++ ventanita/sentenciados/tests.py
@@ -1,3 +1,8 @@
+# Copyright 2015 by AniversarioPeru. All rights reserved.
+# This code is part of the Ventanita distribution and governed by its
+# license. Please see the LICENSE file that should have been included
+# as part of this package.
+
 from django.test import TestCase
 
 # Create your tests here.
