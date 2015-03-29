2f829621459d3406e2eb3a9f7af78fdeb7e6cbb9
aniversarioperu <yoni@aniversarioperu.me>
2015-03-29 21:08:14 +0300

    tests for import

--- ventanita/core/tests/__init__.py
+++ ventanita/core/tests/__init__.py
@@ -2,7 +2,3 @@
 # This code is part of the Ventanita distribution and governed by its
 # license. Please see the LICENSE file that should have been included
 # as part of this package.
-
-from django.test import TestCase
-
-# Create your tests here.
