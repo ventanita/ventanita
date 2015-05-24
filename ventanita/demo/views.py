3d5d5e59136c8ec6694192c1a39dea6692fc3d73
aniversarioperu <yoni@aniversarioperu.me>
2015-05-24 03:12:28 +0300

    minor tweaks

--- ventanita/demo/views.py
+++ ventanita/demo/views.py
@@ -1,7 +1,9 @@
 # Copyright 2015 by AniversarioPeru. All rights reserved.
+# Revisions 2015 copyright by RoxyRocker. All rights reserved.
 # This code is part of the Ventanita distribution and governed by its
 # license. Please see the LICENSE file that should have been included
 # as part of this package.
+
 from django.shortcuts import render
 
 
@@ -11,6 +13,7 @@ def index(request):
 
 def candidatos_con_sentencias(request):
     return render(request, 'demo/1400_candidatos.html')
+
 
 def las_20_agrupaciones(request):
     return render(request, 'demo/20_agrupaciones.html')
